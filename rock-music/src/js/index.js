cb22adedf8ee44da1ef2a4fb3a6444c881faa3da
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-13 01:54:24 +0300

    feat: add footer for preview

--- rock-music/src/js/index.js
+++ rock-music/src/js/index.js
@@ -1,5 +1,8 @@
 import "../style/preview/preview.scss";
 import { makeSound } from "./modules-preview/sound";
-
-makeSound(); // на 3 кнопке включается звуковое сопровождение 
+// import { openRule } from "./modules-preview/open-rule";
+// import { closeRule } from "./modules-preview/close-rule";
+// openRule();
+// closeRule();
+// makeSound(); // на 3 кнопке включается звуковое сопровождение 
 
--- rock-music/src/js/modules-preview/sound.js
+++ rock-music/src/js/modules-preview/sound.js
@@ -1,41 +1,15 @@
 import grom from "../../audio/preview/grom.mp3";
-import voron from "../../audio/preview/voron.mp3";
 import laugh from "../../audio/preview/laugh.mp3";
 
 export function makeSound(){
   function gromSound() {
     const audio = new Audio(grom);
-    setTimeout(() => audio.play(), 3000);
-    setInterval(() => audio.play(), 16000);
+    setTimeout(() => audio.play(), 2000);
   }
-  
-  function voronSound() {
-    const audio = new Audio(voron);
-    setTimeout(() => audio.play(), 5000);
-    setInterval(() => audio.play(), 20000);
-  }
-  
   function laughSound(){
     const audio = new Audio(laugh);
     audio.play();
-    setInterval(() => audio.play(), 12000);
   }
-  
-  const soundon = document.querySelector(".main__item.item-sound");
-  soundon.addEventListener("click", function() {
-    const item = document.querySelector(".main__item.item-sound");
-    item.classList.toggle("sound-off");
-    item.classList.toggle("sound-on");
-    laughSound()
-    gromSound()
-    voronSound()
-  })
-
-  // const soundoff = document.querySelector(".sound-off");
-  // soundoff.addEventListener("click", function() {
-  //   const item = document.querySelector(".main__item.sound-off");
-  //   item.classList.toggle("sound-off");
-  //   console.log("adasds")
-  //   item.classList.toggle("item-sound");
-  // })
+  laughSound();
+  gromSound();
 }

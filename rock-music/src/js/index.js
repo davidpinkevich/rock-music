f79c94fa80184f79aecd64675978d0123595a01b
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-12 21:08:49 +0300

    refactor: structure scss files

--- rock-music/src/js/index.js
+++ rock-music/src/js/index.js
@@ -1,4 +1,4 @@
-import "../style/preview.scss";
+import "../style/preview/preview.scss";
 import { makeSound } from "./modules-preview/sound";
 
 makeSound(); // на 3 кнопке включается звуковое сопровождение 

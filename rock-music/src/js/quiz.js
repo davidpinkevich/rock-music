51472801c7bd61eba3118d647bd4af7d232473ab
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-17 16:45:51 +0300

    feat: add clear local storage score

--- rock-music/src/js/quiz.js
+++ rock-music/src/js/quiz.js
@@ -4,7 +4,7 @@ import { createQuestion } from "./modules-quiz/block-question";
 import { getRandom } from "./modules-quiz/random";
 
 
-//
+localStorage.removeItem("score");
 let rand = getRandom(6);
 createBlock(0)
 createQuestion(0,rand[0])

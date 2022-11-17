8526ca664422ecf5ebdcd3cbc168998c0db6893b
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-17 19:24:22 +0300

    feat: add change lang for result

--- rock-music/src/js/result.js
+++ rock-music/src/js/result.js
@@ -1,4 +1,15 @@
 import "../style/result/result.scss";
 import { createResult } from "./modules-result/create-result";
+import { ruRes } from "./modules-result/ru-res";
+import { engRes } from "./modules-result/eng-res";
 
+
+
+let score = localStorage.getItem("lang");
+
+if(score == "eng") {
+  engRes();
+} else {
+  ruRes();
+}
 createResult();

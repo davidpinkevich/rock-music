2f990ad9e2c7547d4edf51ed4810d8c3f13a422b
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-18 15:58:25 +0300

    feat: start open and close fallery

--- rock-music/src/js/index.js
+++ rock-music/src/js/index.js
@@ -4,11 +4,21 @@ import { closeRule } from "./modules-preview/close-rule";
 import { changeLang } from "./modules-preview/language-prew";
 import { engVersion } from "./modules-preview/eng-verion";
 import { ruVersion } from "./modules-preview/ru-version";
-//
-// changeLang();
+import { openGallery } from "./modules-preview/open-gallery";
+import { closeGallery } from "./modules-preview/close-gallery";
+import { createSlot } from "./modules-preview/create-slot";
+import { songSwith } from "./modules-preview/song-switcher";
+
+closeGallery();
 openRule();
 closeRule();
+songSwith();
 
+const openGall = document.querySelector(".items");
+openGall.addEventListener("click", () => {
+  openGallery();
+  createSlot(0,0);
+})
 const lang = document.querySelector(".main__item-info.lang");
 if(localStorage.getItem("lang") == "eng") {
   lang.classList.add("eng");
@@ -19,5 +29,7 @@ if(localStorage.getItem("lang") == "eng") {
 }
 
 lang.addEventListener("click", changeLang);
+
+console.log("Все пункты выполнены, 270");
 
 

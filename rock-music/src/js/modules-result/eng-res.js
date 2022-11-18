0fd1097092b5f793a614eeeb0bb4ff6bfb37957d
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-18 16:03:01 +0300

    feat: add eng version if result max

--- rock-music/src/js/modules-result/eng-res.js
+++ rock-music/src/js/modules-result/eng-res.js
@@ -3,12 +3,16 @@ export function engRes(){
   score.innerHTML = "Your result:<span></span>";
 
   const scoreText = document.querySelector(".score__text");
-  scoreText.innerHTML = "You're almost a King... But you need 30 shots of rum, try again...";
+  if(score == 30) {
+    scoreText.innerHTML ="Bravo! You won, but the King is still alone...";
+  } else {
+    scoreText.innerHTML = "You're almost a King... But you need 30 shots of rum, try again...";
+  };
+  
 
   const btnRestart = document.querySelector(".btn__restart");
   btnRestart.innerHTML = "<p>Restart</p>";
 
   const btnHome = document.querySelector(".btn__main-menu");
   btnHome.innerHTML = "<p>At home</p>";
-  
 }

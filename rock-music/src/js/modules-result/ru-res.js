a67fa1a7bd513fa39e1e670ca823bc4899b21a32
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-18 16:03:24 +0300

    feat: add ru version if result max

--- rock-music/src/js/modules-result/ru-res.js
+++ rock-music/src/js/modules-result/ru-res.js
@@ -3,7 +3,11 @@ export function ruRes(){
   score.innerHTML = "Твой результат:<span></span>";
 
   const scoreText = document.querySelector(".score__text");
-  scoreText.innerHTML = "Ты почти Король... Но нужно 30 рюмок рома, попробуй ещё раз...";
+  if(score == 30) {
+    scoreText.innerHTML ="Браво! Ты победил, но Король всё равно один...";
+  } else {
+    scoreText.innerHTML = "Ты почти Король... Но нужно 30 рюмок рома, попробуй ещё раз...";
+  };
 
   const btnRestart = document.querySelector(".btn__restart");
   btnRestart.innerHTML = "<p>Заново</p>";

31a8e333aab510bdac34f11fb7873e47e4e4cbd0
davidpinkevich <davidpinkevich95@gmail.com>
2022-11-25 22:12:21 +0300

    fix: info result and rules

--- rock-music/src/js/modules-result/eng-res.js
+++ rock-music/src/js/modules-result/eng-res.js
@@ -2,8 +2,9 @@ export function engRes(){
   const score = document.querySelector(".score__info");
   score.innerHTML = "Your result:<span></span>";
 
+  const result = localStorage.getItem("score");
   const scoreText = document.querySelector(".score__text");
-  if(score == 30) {
+  if(result == 30) {
     scoreText.innerHTML ="Bravo! You won, but the King is still alone...";
   } else {
     scoreText.innerHTML = "You're almost a King... But you need 30 shots of rum, try again...";
--- rock-music/src/js/modules-result/ru-res.js
+++ rock-music/src/js/modules-result/ru-res.js
@@ -2,8 +2,9 @@ export function ruRes(){
   const score = document.querySelector(".score__info");
   score.innerHTML = "Твой результат:<span></span>";
 
+  const result = localStorage.getItem("score");
   const scoreText = document.querySelector(".score__text");
-  if(score == 30) {
+  if(result == 30) {
     scoreText.innerHTML ="Браво! Ты победил, но Король всё равно один...";
   } else {
     scoreText.innerHTML = "Ты почти Король... Но нужно 30 рюмок рома, попробуй ещё раз...";
